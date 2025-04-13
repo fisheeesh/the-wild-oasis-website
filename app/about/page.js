92e4018c893e016584aebe11eb761dbc8589fc3e
fisheeesh <swanphyo444@gmail.com>
2025-04-13 23:58:34 +0700

    chore: created root layout

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,6 @@ import Navigation from '../components/Navigation'
 export default function Page() {
     return (
         <div>
-            <Navigation />
             about
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -5,7 +5,6 @@ export default function Page() {
   return (
     <div>
       <h1>The Wild Oasis. Welcome to paradise.</h1>
-      <Navigation />
     </div>
   )
 }
