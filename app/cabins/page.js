418b94dc4d60bb934632176b1337423beaa4f684
fisheeesh <swanphyo444@gmail.com>
2025-04-14 22:32:18 +0700

    chore: fetching data

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import Navigation from '../components/Navigation'
 
-export default function Page() {
+export default async function Page() {
+    let res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    let data = await res.json()
+
+    console.log(data)
+
     return (
         <div>
             <h1>Cabins Page</h1>
+
+            <ul>
+                {
+                    data.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
