5010dd837e2cb70edb5ac63a60e39a1678e25424
fisheeesh <swanphyo444@gmail.com>
2025-04-16 17:09:35 +0700

    feat: adding nested pages and routes

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -6,8 +6,8 @@ export const metadata = {
 
 export default function Page() {
     return (
-        <div>
-            account
-        </div>
+        <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+            Welcome, Swan Yi Phyo
+        </h2>
     )
 }
