830c1ace5aec3ebf843a6d4c5b9999a82fb9d76f
fisheeesh <swanphyo444@gmail.com>
2025-04-16 16:13:57 +0700

    style: styled root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import '@/app/_styles/globals.css'
-import Navigation from '@/app/_components/Navigation'
-import Logo from '@/app/_components/Logo'
 import { Josefin_Sans } from 'next/font/google'
+import Header from './_components/Header'
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -23,14 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>
-          {children}
-        </main>
+      <body className={`bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased ${josefin.className}`}>
+        <Header />
+        <div className='flex-1 px-8 py-12'>
+          <main className='max-w-7xl mx-auto'>
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   )
