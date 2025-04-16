20b195a6dd759f3b11c96c4d377902ff181ef31d
fisheeesh <swanphyo444@gmail.com>
2025-04-16 23:58:49 +0700

    chore: setting up supabase client

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,4 +1,5 @@
 import { eachDayOfInterval } from 'date-fns';
+import { supabase } from './supabase';
 
 /////////////
 // GET
@@ -10,8 +11,8 @@ export async function getCabin(id) {
     .eq('id', id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
+  //? For testing
+  //? await new Promise((res) => setTimeout(res, 1000));
 
   if (error) {
     console.error(error);
@@ -48,7 +49,7 @@ export const getCabins = async function () {
   return data;
 };
 
-// Guests are uniquely identified by their email address
+//? Guests are uniquely identified by their email address
 export async function getGuest(email) {
   const { data, error } = await supabase
     .from('guests')
@@ -56,7 +57,7 @@ export async function getGuest(email) {
     .eq('email', email)
     .single();
 
-  // No error here! We handle the possibility of no guest in the sign in callback
+  //? No error here! We handle the possibility of no guest in the sign in callback
   return data;
 }
 
@@ -78,7 +79,7 @@ export async function getBooking(id) {
 export async function getBookings(guestId) {
   const { data, error, count } = await supabase
     .from('bookings')
-    // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
+    //? We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
     .select(
       'id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)'
     )
@@ -98,7 +99,7 @@ export async function getBookedDatesByCabinId(cabinId) {
   today.setUTCHours(0, 0, 0, 0);
   today = today.toISOString();
 
-  // Getting all bookings
+  //? Getting all bookings
   const { data, error } = await supabase
     .from('bookings')
     .select('*')
@@ -110,7 +111,7 @@ export async function getBookedDatesByCabinId(cabinId) {
     throw new Error('Bookings could not get loaded');
   }
 
-  // Converting to actual dates to be displayed in the date picker
+  //? Converting to actual dates to be displayed in the date picker
   const bookedDates = data
     .map((booking) => {
       return eachDayOfInterval({
@@ -164,7 +165,7 @@ export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from('bookings')
     .insert([newBooking])
-    // So that the newly created object gets returned!
+    //? So that the newly created object gets returned!
     .select()
     .single();
 
@@ -179,7 +180,7 @@ export async function createBooking(newBooking) {
 /////////////
 // UPDATE
 
-// The updatedFields is an object which should ONLY contain the updated data
+//? The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
     .from('guests')
