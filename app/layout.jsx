969817c50e1bb3e3ec81665986eca00569974882
fisheeesh <swanphyo444@gmail.com>
2025-04-16 16:06:07 +0700

    perf: loading and optimizing fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import '@/app/_styles/globals.css'
 import Navigation from '@/app/_components/Navigation'
 import Logo from '@/app/_components/Logo'
+import { Josefin_Sans } from 'next/font/google'
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   // title: 'The Wild Oasis',
@@ -11,13 +17,13 @@ export const metadata = {
   },
   //? for SEO, page description like a meta tag in the head
   description: "Luxurious Cabins Hotel, located in the heart of the Thailand Chiang Rai, surrounded by beautiful mountains and dark forests.",
-  
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}>
         <header>
           <Logo />
           <Navigation />
