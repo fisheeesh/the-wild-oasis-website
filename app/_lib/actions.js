446028d6b1a0ba9d765eb22c4135cb2d0ad8cca1
fisheeesh <swanphyo444@gmail.com>
2025-04-20 15:49:39 +0700

    auth: implement signout feature

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,18 @@
 "use server"
 
-import { signIn } from "./auth"
+import { signIn, signOut } from "./auth"
+
+//$ server actions can even be called from client components and will also be executed on the server.
 
 export const signInAction = async () => {
     //? After user successfully sign in, redirect to account page.
     await signIn('google', {
         redirectTo: '/account'
+    })
+}
+
+export const signOutAction = async () => {
+    await signOut({
+        redirectTo: '/'
     })
 }
