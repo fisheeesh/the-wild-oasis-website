77222bceb06589934601e5f937480dc1a028ea01
fisheeesh <swanphyo444@gmail.com>
2025-04-20 15:19:41 +0700

    refactor: refactor auth.js

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,18 +1,9 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
 
 export const authConfig = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-    ],
+    providers: [Google],
     callbacks: {
-        /**
-         * ? Check whether, user is authorized or not.
-         * ? This callback will be called whenever a user tries to access a protected route which we handle in middleware.js
-         */
         authorized({ auth, request }) {
             return !!auth?.user
         }
