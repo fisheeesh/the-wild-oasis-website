2c65349fdeb9b1841c7aa37fe27b08a38c7f1eb6
fisheeesh <swanphyo444@gmail.com>
2025-04-21 22:38:04 +0700

    feat: updated reservation by id using server action

---
+++ app/_components/UpdateReservationForm.jsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useFormStatus } from 'react-dom'
+import { updateReservationAction } from '../_lib/actions'
+import SpinnerMini from './SpinnerMini'
+
+export default function UpdateReservationForm({ booking, cabin }) {
+    const { id, observations, numGuest } = booking
+    const { maxCapacity } = cabin
+
+    return (
+        <form action={updateReservationAction} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+            <div className="space-y-2">
+                <label htmlFor="numGuest">How many guests?</label>
+                <select
+                    defaultValue={numGuest}
+                    name="numGuest"
+                    id="numGuest"
+                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+                    required
+                >
+                    <option value="" key="">
+                        Select number of guests...
+                    </option>
+                    {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
+                        <option value={x} key={x}>
+                            {x} {x === 1 ? "guest" : "guests"}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="space-y-2">
+                <label htmlFor="observations">
+                    Anything we should know about your stay?
+                </label>
+                <textarea
+                    id='observations'
+                    placeholder="Please let us know if you have any special requests or needs."
+                    maxLength={200}
+                    rows={5}
+                    defaultValue={observations}
+                    name="observations"
+                    className="px-5 resize-none py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+                />
+            </div>
+
+            <div className="flex justify-end items-center gap-6">
+                <UpdateButton />
+                <input type="hidden" name="bookingId" value={id} />
+            </div>
+        </form>
+    )
+}
+
+function UpdateButton() {
+    const { pending } = useFormStatus()
+
+    return (
+        <button disabled={pending} className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+            {
+                pending ?
+                    <div className="flex items-center gap-2">
+                        <SpinnerMini /> Updating...
+                    </div> :
+                    'Update reservation'
+            }
+        </button>
+    )
+}
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
 import { getBookings } from "./data-service"
+import { redirect } from "next/navigation"
 
 /**
  * $ server actions can even be called from client components and will also be executed on the server.
@@ -66,6 +67,36 @@ export const deleteReservationAction = async (bookingId) => {
     revalidatePath('/account/reservations')
 }
 
+export const updateReservationAction = async (formData) => {
+    const session = await auth()
+    if (!session) throw new Error('You must be logged in.')
+
+    const numGuest = formData.get('numGuest')
+    const observations = formData.get('observations')
+    const bookingId = formData.get('bookingId')
+
+    const bookings = await getBookings(session.user.guestId)
+    const guestBookingIds = bookings.map(booking => booking.id)
+
+    if (!guestBookingIds.includes(bookingId)) throw new Error('You are not allowed to update this booking.')
+
+    const updatedData = { numGuest, observations }
+
+    const { error } = await supabase
+        .from('bookings')
+        .update(updatedData)
+        .eq('id', bookingId)
+        .select()
+        .single();
+
+    if (error) {
+        throw new Error('Booking could not be updated');
+    }
+
+    //? 
+    redirect('/account/reservations')
+}
+
 // curl 'http://localhost:3000/account/reservations' \
 //   -H 'Accept: text/x-component' \
 //   -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,my;q=0.7' \
@@ -84,4 +115,23 @@ export const deleteReservationAction = async (bookingId) => {
 //   -H 'sec-ch-ua-mobile: ?0' \
 //   -H 'sec-ch-ua-platform: "macOS"' \
 //   --data-raw '[460]'
+
+// curl 'http://localhost:3000/account/reservations/edit/464' \
+//   -H 'Accept: text/x-component' \
+//   -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,my;q=0.7' \
+//   -H 'Connection: keep-alive' \
+//   -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryP6bhiQB9RXLeTWjM' \
+//   -b '__next_hmr_refresh_hash__=794877a4bc48c85b2841f8d9e4d60f9c62357935f1640452; __next_hmr_refresh_hash__=b412068c5e5b19c3e8372091b7ba2b5962357935f1640452; authjs.csrf-token=54e878c47ad806d2cf896ced002df8275e6ec7187ef40b2366ef5c9115f00900%7Ca0c5091c7fdd0954819a867e4094524ca34905283d3e2a5f113afb640227f8f2; authjs.callback-url=http%3A%2F%2Flocalhost%3A3000; authjs.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiSUpMZndzanEwMzFuVWE0TVYtRGpLdmdYaWtsYUdMdHBGaTl0T2haWGJRc0haNVRSX2RwWGgtUmdQYm5ER3FRczRRM0laanM4akk4amVvQUVpdmdwbmcifQ..6Sw81W41zNo4mIJhFEj9nQ.edIqZ6z_x-Vpn080Tx8RGcsJaaPIjtH0VxwAtd9XKTM75KZJM7ajUEUZZ-Llo1N-TxVs5j8l9laTS3y38x9zxS-kUQOnmHURoKPL-KVTDS3NtihWOGywBNvXjD37u-zRQTVUgpRlof4S6fhMaIA3J2jfPY5tvLd7blQkI5D27OYOsPHBGUP_ysCKHaFGmun5GcqubkTP9cVGyQ9p3EBnIp4pWX4PeON5_JKQ7sz_eqGeXm06zVypwVcH62nfVrWouA8P3KUeR9dHWz4B0G62my6xQ1reQf7vpzN50R1kXVpu-ObeWoC9Khsn0mWC7TNioPCV8C30xaFcPLYWba85ntyi5f5NAGpa8CaUscrdmmNvmgVJEwPWH97ixAeCdWPm68PG1uAHA3jCXVgOidMUyw.YTdVxLYcAf1JrlKgvCcM3XA38TEkAfsF9EuXNoyG03w; __next_hmr_refresh_hash__=b00971b7abe11db577a9ba95c120e51762357935f1640452' \
+//   -H 'Next-Action: 7f84f84bd50b05b31f6ddc283cede15e8723bab30b' \
+//   -H 'Next-Router-State-Tree: %5B%22%22%2C%7B%22children%22%3A%5B%22account%22%2C%7B%22children%22%3A%5B%22reservations%22%2C%7B%22children%22%3A%5B%22edit%22%2C%7B%22children%22%3A%5B%5B%22reservationId%22%2C%22464%22%2C%22d%22%5D%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%2C%22%2Faccount%2Freservations%2Fedit%2F464%22%2C%22refresh%22%5D%7D%5D%7D%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%5D%7D%2Cnull%2Cnull%2Ctrue%5D' \
+//   -H 'Origin: http://localhost:3000' \
+//   -H 'Referer: http://localhost:3000/account/reservations/edit/464' \
+//   -H 'Sec-Fetch-Dest: empty' \
+//   -H 'Sec-Fetch-Mode: cors' \
+//   -H 'Sec-Fetch-Site: same-origin' \
+//   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' \
+//   -H 'sec-ch-ua: "Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"' \
+//   -H 'sec-ch-ua-mobile: ?0' \
+//   -H 'sec-ch-ua-platform: "macOS"' \
+//   --data-raw $'------WebKitFormBoundaryP6bhiQB9RXLeTWjM\r\nContent-Disposition: form-data; name="1_numGuest"\r\n\r\n2\r\n------WebKitFormBoundaryP6bhiQB9RXLeTWjM\r\nContent-Disposition: form-data; name="1_observations"\r\n\r\nGf nae tu tu lr ml\r\n------WebKitFormBoundaryP6bhiQB9RXLeTWjM\r\nContent-Disposition: form-data; name="1_bookingId"\r\n\r\n464\r\n------WebKitFormBoundaryP6bhiQB9RXLeTWjM\r\nContent-Disposition: form-data; name="0"\r\n\r\n["$K1"]\r\n------WebKitFormBoundaryP6bhiQB9RXLeTWjM--\r\n'
 
