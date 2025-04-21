3a5c3715bdd64756ebb9b03d2d38329cb8376a09
fisheeesh <swanphyo444@gmail.com>
2025-04-22 00:19:17 +0700

    feat: showing toast notifications after actions

--- app/_components/ReservationList.jsx
+++ app/_components/ReservationList.jsx
@@ -7,7 +7,10 @@ import { deleteReservationAction } from '../_lib/actions'
 export default function ReservationList({ bookings }) {
     /**
      * ? useOptimistic hook takes 2 arguments.
-     * ? 1. 
+     * ? 1. The data we want to be optimistic about.
+     * ? 2. A function that takes the current data and anything we pass into async func and returns the new data.
+     * ? It returns 2 values which are the current data(optimistic) and the function to update the data.
+     * ? Like useReducer hook, optimistic function will be called when we use optimisticDelete function.
      */
     const [optimisticBookings, optimisticDelete] = useOptimistic(
         bookings,
