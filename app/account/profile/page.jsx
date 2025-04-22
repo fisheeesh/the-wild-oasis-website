5255070a5be6b3cb3e67124c9ddfced6a240dfa8
fisheeesh <swanphyo444@gmail.com>
2025-04-22 17:20:12 +0700

    refactor: make cutomize theme

--- app/account/profile/page.jsx
+++ app/account/profile/page.jsx
@@ -13,7 +13,7 @@ export default async function Page() {
     const guest = await getGuest(session.user.email)
 
     return (
-        <div>
+        <div className="">
             <h2 className="font-semibold text-2xl text-accent-400 mb-4">
                 Update your guest profile
             </h2>
