5008a9fd4c39ca2e0aeadcc7d8adbc786e1eb7e7
fisheeesh <swanphyo444@gmail.com>
2025-06-08 01:43:33 +0700

    fix: reminder design fixed

--- app/_components/ReservationReminder.jsx
+++ app/_components/ReservationReminder.jsx
@@ -5,19 +5,22 @@ import { format } from 'date-fns';
 import { useReservation } from './ReservationContext';
 
 function ReservationReminder() {
-  // CHANGE
-  const { range, resetRange } = useReservation()
+  const { range, resetRange } = useReservation();
 
   if (!range.from || !range.to) return null;
 
   return (
-    <div className='fixed bottom-6 w-fit left-16 right-16 sm:right-auto sm:left-1/2 sm:-translate-x-1/2  py-5 px-8 rounded-full bg-accent-500 text-primary-800 font-semibold justify-center shadow-xl shadow-slate-900 flex gap-8 items-center'>
+    <div className="fixed bottom-6 left-0 right-0 mx-4 md:left-1/2 md:right-auto md:-translate-x-1/2 md:w-fit py-5 px-6 bg-accent-500 text-primary-800 font-semibold shadow-xl shadow-slate-900 flex gap-6 items-center justify-between">
       <p className='text-sm lg:text-base'>
-        <span>👋</span> Don'f forget to reserve your dates <br /> from{' '}
+        <span>👋</span> Don’t forget to reserve your dates <br /> from{' '}
         {format(new Date(range.from), 'MMM dd yyyy')} to{' '}
         {format(new Date(range.to), 'MMM dd yyyy')}
       </p>
-      <button type='button' onClick={resetRange} className='rounded-full cursor-pointer p-1 hover:bg-accent-600 transition-all'>
+      <button
+        type='button'
+        onClick={resetRange}
+        className='rounded-full cursor-pointer p-1 hover:bg-accent-600 transition-all'
+      >
         <XMarkIcon className='h-5 w-5' />
       </button>
     </div>
